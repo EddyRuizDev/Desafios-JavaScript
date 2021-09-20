2a7ed4314d6e2db49f44fe77676b9e8fd5f6d76a
Edgar Ruiz <heddy01.02@gmail.com>
2021-09-20 15:38:15 -0300

    desafio subido

---
+++ Primer Desafio/app.js
@@ -0,0 +1,8 @@
+
+let nombre = prompt("Ingrese nombre: ");
+let apellido = prompt("Ingrese apellido: ");
+let nacimiento = Number(prompt("¿En que año naciste?: "));
+
+console.log("Nombre: " +nombre);
+console.log("Apellido: " +apellido);
+console.log("Tu edad es: " + (2021-nacimiento));
